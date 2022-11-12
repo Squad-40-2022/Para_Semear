e2df315c6cbccd6020ccaaf322ffb5d85ec656d2
Bea Cavalheiro <biacavalheiro2bia@gmail.com>
2022-11-12 14:19:14 -0300

    inicio modais

---
+++ frontend/parasemear/src/components/Cards/modais.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+
+export default function modais() {
+  return (
+    <View>
+      <Text>modais</Text>
+    </View>
+  )
+}
+export default function modais() {
+    return (
+      <View>
+        <Text>modais</Text>
+      </View>
+    )
+  }
+  export default function modais() {
+    return (
+      <View>
+        <Text>modais</Text>
+      </View>
+    )
+  }
+  export default function Modal1() {
+    return (
+        <div class="modal modal-dialog-scrollable" id="staticBackdrop001" data-bs-backdrop="static" data-bs-keyboard="false"
+        tabindex="-1" aria-labelledby="staticBackdropLabel001" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel001">Verde Oliva</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <table>
+                        <tr>
+                            <td> Razão Social:</td>
+                            <td> Verde Oliva ME</td>
+                        </tr>
+                        <tr>
+                            <td> Nome Fantasia:</td>
+                            <td> Verde Oliva </td>
+                        </tr>
+                        <tr>
+                            <td> CNPJ:</td>
+                            <td> 000258021/0001</td>
+                        </tr>
+                        <tr>
+                            <td> Endereço:</td>
+                            <td> Coronel de matos, 507 </td>
+                            <td>Bairro:</td>
+                            <td>Flores do nascer</td>
+                        </tr>
+                        <tr>
+                            <td> Cep:</td>
+                            <td> 06915-169</td>
+                            <td> Cidade:</td>
+                            <td> Rio de Janeiro</td>
+                            <td>UF:</td>
+                            <td>RJ</td>
+                        </tr>
+                        <tr>
+                            <td>email:</td>
+                            <td colspan="2">faleconosco@verdeoliva.com.br</td>
+                        </tr>
+                        <tr>
+                            <td rowspan="2">Telefones:</td>
+                            <td> (21) 2648-7898</td>
+                        </tr>
+                        <tr>
+                            <td> (21) 96587-3636</td>
+                        </tr>
+                        <tr>
+
+                        </tr>
+                    </table>
+                </div>
+                <div class="modal-footer">
+
+                    <a href="./dados.html"><button type="submit" class="btn bt-inst tx">Alterar</button></a>
+                    <button type="button" class="btn bt-inst tx" data-bs-dismiss="modal">Finalizar</button>
+                </div>
+            </div>
+        </div>
+    </div> 
+    )
+  }
+  export default function modais() {
+    return (
+      <View>
+        <Text>modais</Text>
+      </View>
+    )
+  }
+  export default function modais() {
+    return (
+      <View>
+        <Text>modais</Text>
+      </View>
+    )
+  }
