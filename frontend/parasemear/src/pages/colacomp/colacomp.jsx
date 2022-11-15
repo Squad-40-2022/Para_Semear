88acc9cb164ce9117762e4954d3dfde80408d863
SannayDuarte <Sannaylinnyd@gmail.com>
2022-11-14 23:38:55 -0300

    commit col acom inst

---
+++ frontend/parasemear/src/pages/colacomp/colacomp.jsx
@@ -0,0 +1,201 @@
+import React from 'react'
+import user from '../IMG/user.png';
+import user30 from '../IMG/user-30px.png';
+
+export default function ColAcom() {
+    return (
+
+        <div className="AcompCol">
+            <div className='acomp'>
+                <main class="container conteudo my-4 p-4">
+                    <div class="border-bottom">
+                        <div class="front">
+                            <img src={user} className="Ong-logo" alt="ong" />
+                        </div>
+                        <h2>Maria Silva</h2>
+                    </div>
+
+                </main>
+                <div class="container">
+                    <div class="mb-4 row justify-content-between">
+                        <aside class="col-3 conteudo">
+                            <div class="row px-5">
+                                <button class="btn bt-menu mt-5"><a href="colab_acom.html" class="tx">Publicações</a></button>
+                                <button class="btn bt-menu"><a href="./login_colab/doacao.html" class="tx">Nova
+                                    doação</a></button>
+                                <button class="btn bt-menu"><a href="./login_colab/colab_inst.html" class="tx">Minhas
+                                    instituições</a></button>
+                                <button class="btn bt-menu"><a href="./login_colab/colab_rel.html"
+                                    class="tx">Relatório 2021 disponível</a></button>
+                                <button class="btn bt-menu"><a href="./login_colab/colab_conf.html" class="tx">Configurações
+                                    e privacidade</a></button>
+                            </div>
+                        </aside>
+                        <section class="col-xl-8 col-12 conteudo cont-fixed p-5">
+                            <article class="border-bottom">
+                                <div class="d-flex mb-3">
+                                    <div class="front">
+                                        <img src={user30} className="Ong-logo" alt="ong" />
+                                    </div>
+                                    <h6 class="px-4">Nome Instituição</h6>
+                                </div>
+                                <div>
+                                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, nobis. Neque
+                                        necessitatibus
+                                        maiores, officia, aut ullam atque qui molestiae tempora praesentium facere animi sit
+                                        consequuntur ratione? Similique est sed officia.</p>
+                                </div>
+                                <div class="border-bottom border-top d-flex">
+                                    <button class="btn">        </button>
+                                    <button class="btn">        </button>
+                                    <button class="btn">        </button>
+
+                                </div>
+                                <div>
+                                    <div class="m-4">
+                                        <div class="d-flex mb-2">
+
+                                            <h6 class="px-4">Nome Usuário</h6>
+                                        </div>
+                                        <div class="coment">
+                                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, nobis.
+                                                Neque
+                                                necessitatibus
+                                                maiores, officia, aut ullam atque qui molestiae tempora praesentium facere
+                                                animi sit
+                                                consequuntur ratione? Similique est sed officia.</p>
+                                        </div>
+                                    </div>
+                                    <div class="m-4">
+                                        <div class="d-flex mb-2">
+
+                                            <h6 class="px-4">Nome Usuário</h6>
+                                        </div>
+                                        <div class="coment">
+                                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, nobis.
+                                                Neque
+                                                necessitatibus
+                                                maiores, officia, aut ullam atque qui molestiae tempora praesentium facere
+                                                animi sit
+                                                consequuntur ratione? Similique est sed officia.</p>
+                                        </div>
+                                    </div>
+                                    <div class="mb-3">
+                                        <textarea class="coment-form" id="exampleFormControlTextarea1" rows="3" placeholder="Escreva seu comentário..."></textarea>
+                                    </div>
+                                </div>
+                            </article>
+                            <article class="border-bottom">
+                                <div class="d-flex mb-3">
+                                    <div class="front">
+                                        <img src={user30} className="Ong-logo" alt="ong" />
+                                    </div>
+                                    <h6 class="px-4">Nome Instituição</h6>
+                                </div>
+                                <div>
+                                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, nobis. Neque
+                                        necessitatibus
+                                        maiores, officia, aut ullam atque qui molestiae tempora praesentium facere animi sit
+                                        consequuntur ratione? Similique est sed officia.</p>
+                                </div>
+                                <div class="border-bottom border-top d-flex">
+                                    <button class="btn">   </button>
+                                    <button class="btn">   </button>
+                                    <button class="btn">   </button>
+
+                                </div>
+                                <div>
+                                    <div class="m-4">
+                                        <div class="d-flex mb-2">
+
+                                            <h6 class="px-4">Nome Usuário</h6>
+                                        </div>
+                                        <div class="coment">
+                                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, nobis.
+                                                Neque
+                                                necessitatibus
+                                                maiores, officia, aut ullam atque qui molestiae tempora praesentium facere
+                                                animi sit
+                                                consequuntur ratione? Similique est sed officia.</p>
+                                        </div>
+                                    </div>
+                                    <div class="m-4">
+                                        <div class="d-flex mb-2">
+
+                                            <h6 class="px-4">Nome Usuário</h6>
+                                        </div>
+                                        <div class="coment">
+                                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, nobis.
+                                                Neque
+                                                necessitatibus
+                                                maiores, officia, aut ullam atque qui molestiae tempora praesentium facere
+                                                animi sit
+                                                consequuntur ratione? Similique est sed officia.</p>
+                                        </div>
+                                    </div>
+                                    <div class="mb-3">
+                                        <textarea class="coment-form" id="exampleFormControlTextarea1" rows="3" placeholder="Escreva seu comentário..."></textarea>
+                                    </div>
+                                </div>
+                            </article>
+                            <article class="border-bottom">
+                                <div class="d-flex mb-3">
+                                    <div class="front">
+                                        <img src={user30} className="Ong-logo" alt="ong" />
+                                    </div>
+                                    <h6 class="px-4">Nome Instituição</h6>
+                                </div>
+                                <div>
+                                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, nobis. Neque
+                                        necessitatibus
+                                        maiores, officia, aut ullam atque qui molestiae tempora praesentium facere animi sit
+                                        consequuntur ratione? Similique est sed officia.</p>
+                                </div>
+                                <div class="border-bottom border-top d-flex">
+                                    <button class="btn">   </button>
+                                    <button class="btn">   </button>
+                                    <button class="btn">   </button>
+                                </div>
+                                <div>
+                                    <div class="m-4">
+                                        <div class="d-flex mb-2">
+
+                                            <h6 class="px-4">Nome Usuário</h6>
+                                        </div>
+                                        <div class="coment">
+                                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, nobis.
+                                                Neque
+                                                necessitatibus
+                                                maiores, officia, aut ullam atque qui molestiae tempora praesentium facere
+                                                animi sit
+                                                consequuntur ratione? Similique est sed officia.</p>
+                                        </div>
+                                    </div>
+                                    <div class="m-4">
+                                        <div class="d-flex mb-2">
+
+                                            <h6 class="px-4">Nome Usuário</h6>
+                                        </div>
+                                        <div class="coment">
+                                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, nobis.
+                                                Neque
+                                                necessitatibus
+                                                maiores, officia, aut ullam atque qui molestiae tempora praesentium facere
+                                                animi sit
+                                                consequuntur ratione? Similique est sed officia.</p>
+                                        </div>
+                                    </div>
+                                    <div class="mb-3">
+                                        <textarea class="coment-form" id="exampleFormControlTextarea1" rows="3" placeholder="Escreva seu comentário..."></textarea>
+                                    </div>
+                                </div>
+                            </article>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+
+}
