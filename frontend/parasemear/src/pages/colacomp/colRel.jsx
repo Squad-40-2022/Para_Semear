fad060a7a0d256663743e73e8f0e6c50e8c4a9b7
SannayDuarte <Sannaylinnyd@gmail.com>
2022-11-14 23:02:28 -0300

    commit relat col

---
+++ frontend/parasemear/src/pages/colacomp/colRel.jsx
@@ -0,0 +1,131 @@
+import React from 'react'
+import user from '../IMG/user.png';
+import dow from '../IMG/imagens.colaboradores/pdf-download-2617 (1).png';
+
+export default function ColRel() {
+    return (
+
+        <div className='relat'>
+            <main class="container conteudo my-4 p-4">
+                <div class="border-bottom">
+                    <div class="front">
+                        <img src={user} className="Ong-logo" alt="ong" />
+                    </div>
+                    <h2>Maria Silva</h2>
+                </div>
+
+            </main>
+            <div class="container">
+                <div class="mb-4 row justify-content-between">
+                    <aside class="col-3 conteudo">
+                        <div class="row px-5">
+                            <button class="btn bt-menu mt-5"><a href="../colab_acom.html" class="tx">Publicações</a></button>
+                            <button class="btn bt-menu"><a href="doacao.html" class="tx">Nova
+                                doação</a></button>
+                            <button class="btn bt-menu"><a href="colab_inst.html" class="tx">Minhas
+                                instituições</a></button>
+                            <button class="btn bt-menu"><a href="colab_rel.html"
+                                class="tx">Relatório 2021 disponível</a></button>
+                            <button class="btn bt-menu"><a href="colab_conf.html" class="tx">Configurações
+                                e privacidade</a></button>
+                        </div>
+                    </aside>
+
+                    <section class="col-xl-8 col-12 conteudo cont-fixed p-5 ">
+                        <table class="table">
+                            <h1>Olá, Maria!</h1>
+                            <h5 text primary>Aqui estão os detalhamentos de suas doações :</h5>
+                            <thead>
+                                <tr>
+                                    <th scope="col">TIPO</th>
+                                    <th scope="col">QUANTIDADE</th>
+                                    <th scope="col">INSTITUIÇÃO</th>
+                                    <th scope="col">DATA</th>
+                                    <th scope="col">DESCRIÇÃO</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">LIVRO</th>
+                                    <td>1</td>
+                                    <td>VERDE OLIVA</td>
+                                    <td>15/03</td>
+                                    <td>COLEÇÃO MONTEIRO LOBATO</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">MONETÁRIO</th>
+                                    <td>R$500</td>
+                                    <td>VERDE OLIVA</td>
+                                    <td>14/06</td>
+                                    <td>APOIO PROJETOS</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">ARTE </th>
+                                    <td>1</td>
+                                    <td>VERDE OLIVA</td>
+                                    <td>09/10</td>
+                                    <td>ARTES VISUAIS</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">MONETÁRIO</th>
+                                    <td>R$200</td>
+                                    <td>VERDE OLIVA</td>
+                                    <td>09/10</td>
+                                    <td>APOIO PROJETOS</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">LIVRO</th>
+                                    <td>2</td>
+                                    <td>VERDE OLIVA</td>
+                                    <td>11/11</td>
+                                    <td>COLEÇÃO MACHADO DE ASSIS</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">LIVRO</th>
+                                    <td>1</td>
+                                    <td>VERDE OLIVA</td>
+                                    <td>24/12</td>
+                                    <td>COLEÇÃO JOAQUI M. MACEDO</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">VERBA</th>
+                                    <td>R$250</td>
+                                    <td>VERDE OLIVA</td>
+                                    <td>29/12</td>
+                                    <td>1 LOTE CAMISA UNIFORME</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                        <div classe="container">
+                            <button onclick="window.print()"
+                                class="btn bt-ins">Imprima</button>
+
+                        </div>
+
+
+                        <div>
+                            <p class="fs- color lin">
+
+                            </p>
+                            <div class="front">
+                                <img src={dow} className="Ong-logo" alt="ong" />
+                            </div>
+
+                            <div href="https://drive.google.com/file/d/1K9qXaFN71b4UuFJFZi3eAulVWX4r5huA/view?usp=sharing"
+                                target="_blank">RELATÓRIO </div>
+                        </div>
+
+
+
+                    </section>
+                </div>
+
+
+            </div>
+
+        </div>
+
+    )
+
+}
