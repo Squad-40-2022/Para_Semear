a6c38c841c42dd76fbee53cdc399fbd7932693fd
Bea Cavalheiro <biacavalheiro2bia@gmail.com>
2022-11-15 16:02:25 -0300

    header

--- src/components/estrutura/header.jsx
+++ src/components/estrutura/header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Logo from "../../IMG/Icon-login.png"
+import LogoLogin from "../../IMG/Icon-login.png"
+import Logo from "../../IMG/logo/logo-100px.png"
 import "../../CSS/index.css"
 
 function Header(){
@@ -8,7 +9,7 @@ function Header(){
     <div class="container">
       <div class="row">
         <nav class="navbar navbar-expand-md navbar-light col-10 d-flex align-items-end">
-          <a class="navbar-brand" href="index.html"><img src="../../IMG/logo/logo-100px.png" alt=""/></a>
+          <a class="navbar-brand" href="index.html"><img src={Logo} alt=""/></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId"
             aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -16,10 +17,10 @@ function Header(){
           <div class="collapse navbar-collapse" id="collapsibleNavId">
             <ul class="navbar-nav me-auto mt-2 mt-lg-0">
               <li class="nav-item">
-                <a class="nav-link text-white" href="HTML/colaboradores.html">Colaboradores</a>
+                <a class="nav-link text-white" href="./colaborador">Colaboradores</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link text-white" href="HTML/instituicoes.html">Instituições</a>
+                <a class="nav-link text-white" href="./instituicao">Instituições</a>
               </li>
             </ul>
           </div>
@@ -27,7 +28,7 @@ function Header(){
         
         <div class="col-1 d-flex align-items-end pb-2">
           <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            <img src={Logo} alt=""/>
+            <img src={LogoLogin} alt=""/>
           </button>
           
           <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
