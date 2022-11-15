ce9269a0b0f2aae8a09c1d91b3908e06a771bcc0
SannayDuarte <Sannaylinnyd@gmail.com>
2022-11-14 23:50:17 -0300

    commit up check

--- frontend/parasemear/src/pages/checkout.jsx
+++ frontend/parasemear/src/pages/checkout.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Checkout() {
     return (
 
-        <div class="col-1 d-flex align-items-end pb-2">
+        <div class="col-1 d-flex pb-2">
             <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 <img src="../IMG/Icon-login.png" alt="" />
             </button>
