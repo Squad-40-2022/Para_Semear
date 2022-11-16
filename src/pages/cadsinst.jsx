9705a05bc7a06aa53d2c0468a53abc41a3b8707a
Bea Cavalheiro <biacavalheiro2bia@gmail.com>
2022-11-16 00:19:59 -0300

    Update cadsinst.jsx

--- src/pages/cadsinst.jsx
+++ src/pages/cadsinst.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from '../components/estrutura/header'
+import Footer from '../componentss/estruturas/footer'
 
 export default function cadsinst() {
     return (
